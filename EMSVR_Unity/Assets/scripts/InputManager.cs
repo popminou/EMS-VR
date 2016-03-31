67d727218c57443d75221f314b4df9b26fd97350
Maxim Sucharski <msucharski@amplify.com>
2016-03-31 11:17:02 -0400

    -innput manager and button assignments

---
+++ EMSVR_Unity/Assets/scripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float LST = Input.GetAxis ("LeftShoulderTrigger");
+		float RST = Input.GetAxis ("RightShoulderTrigger");
+		if (LST > 0)
+			Debug.Log ("LST: " + LST);
+		if(RST > 0)
+			Debug.Log ("RST: " + RST);
+
+		if (Input.GetButtonDown ("Fire1"))
+			Debug.Log ("Fire1"); 
+		if (Input.GetButtonDown ("Fire2"))
+			Debug.Log ("Fire2"); 
+	
+	}
+}
