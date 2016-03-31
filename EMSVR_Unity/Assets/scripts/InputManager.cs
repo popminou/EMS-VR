500ef46c93e0d64f8c48b69f1ff465a5a2e78b6c
Maxim Sucharski <msucharski@amplify.com>
2016-03-31 11:47:17 -0400

    -position changes, input to slow or speed up time

--- EMSVR_Unity/Assets/scripts/InputManager.cs
+++ EMSVR_Unity/Assets/scripts/InputManager.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour {
 
+	TimeManager timeManager;
 	// Use this for initialization
 	void Start () {
-	
+		timeManager = GameObject.FindObjectOfType<TimeManager> ();
 	}
 	
 	// Update is called once per frame
@@ -15,13 +16,16 @@ public class InputManager : MonoBehaviour {
 		float RST = Input.GetAxis ("RightShoulderTrigger");
 		if (LST > 0) {
 			Debug.Log ("LST: " + LST);
-		
+			timeManager.SlowTime (LST);
 		}
 		if (RST > 0) {
 			Debug.Log ("RST: " + RST);
+			timeManager.SpeedUpTime (RST);
 		}
-		if (Input.GetButtonDown ("Fire1"))
+		if (Input.GetButtonDown ("Fire1")) {
+			timeManager.TogglePauseTime ();
 			Debug.Log ("Fire1"); 
+		}
 		if (Input.GetButtonDown ("Fire2"))
 			Debug.Log ("Fire2"); 
 	
