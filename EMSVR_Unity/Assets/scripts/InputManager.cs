f7507982234faf6d4fe7eca40807c13576251017
Maxim Sucharski <msucharski@amplify.com>
2016-03-31 11:21:33 -0400

    -input manager added to scene

--- EMSVR_Unity/Assets/scripts/InputManager.cs
+++ EMSVR_Unity/Assets/scripts/InputManager.cs
@@ -13,11 +13,13 @@ public class InputManager : MonoBehaviour {
 
 		float LST = Input.GetAxis ("LeftShoulderTrigger");
 		float RST = Input.GetAxis ("RightShoulderTrigger");
-		if (LST > 0)
+		if (LST > 0) {
 			Debug.Log ("LST: " + LST);
-		if(RST > 0)
+		
+		}
+		if (RST > 0) {
 			Debug.Log ("RST: " + RST);
-
+		}
 		if (Input.GetButtonDown ("Fire1"))
 			Debug.Log ("Fire1"); 
 		if (Input.GetButtonDown ("Fire2"))
