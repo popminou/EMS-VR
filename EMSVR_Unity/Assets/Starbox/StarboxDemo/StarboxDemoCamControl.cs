ce2af59d566b788a1db1b450cfac979273fb8647
Michael Susol <msusol@amplify.com>
2016-04-01 13:01:41 -0400

    Added star field and sdfx

---
+++ EMSVR_Unity/Assets/Starbox/StarboxDemo/StarboxDemoCamControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StarboxDemoCamControl : MonoBehaviour {
+	float yaw = 0f;
+	float pitch = 0f;
+
+	void Update() {
+		yaw += Input.GetAxis("Horizontal") * 360 * Time.deltaTime;
+		pitch = Mathf.Clamp(pitch - Input.GetAxis("Vertical") * 180 * Time.deltaTime, -90, 90);
+
+		transform.rotation = Quaternion.Euler(pitch, yaw, 0);
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(20, Screen.height-20, 400, 20),
+		          "Use arrow keys to rotate camera.");
+	}
+}
